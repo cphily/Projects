06c56799dbeb04deff7f513dca080e1744f16e5e
cphily <philpot_colin@students.columbusstate.edu>
2025-01-20 22:20:33 -0500

    Add files via upload

---
+++ First.java
@@ -0,0 +1,13 @@
+// Author: Your Name
+// This program displays a greeting message, job title, rationale, and median salary.
+
+public class First {
+    public static void main(String[] args) {
+        // Print out the required information
+        System.out.println("Hola mundo!");
+        System.out.println("Job title: Computer Systems Analysts");
+        System.out.println("Why: Passion problem solving and discovering innovative ways to improve efficiency");
+        System.out.println("Median salary: $103,800");
+    }
+}
+
